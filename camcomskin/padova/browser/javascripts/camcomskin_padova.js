2014d047d18329aade2ac1db8f5841e6cff7cbfa
Andrea Cecchi <andrea.cecchi85@gmail.com>
2015-09-25 16:36:19 +0200

    fix secondary menu

--- camcomskin/padova/browser/javascripts/camcomskin_padova.js
+++ camcomskin/padova/browser/javascripts/camcomskin_padova.js
@@ -1,12 +1,19 @@
 (function($) {
   $(document).ready(function() {
     $(".secondaryMenu [data-toggle=popover]").popover({html:true});
-    // $('li.secondaryMenuItem').on("blur", "a", function(e) {
-    //   var parent_id = $(this).parents('li').attr('id');
-    //   if ($(e.relatedTarget).parents("li.secondaryMenuItem").attr('id') !== parent_id) {
-    //     $("#" + parent_id + " [data-toggle=popover]").popover('hide');
-    //     $(e.relatedTarget).focus();
-    //   }
-    // });
+    $(document).click(function(event) {
+      if(!$(event.target).closest('.secondaryMenuItem').length) {
+        $(".secondaryMenu [data-toggle=popover]").popover('hide');
+      }
+    });
+    $("li.secondaryMenuItem a.popovers").click(function(e) {
+      if ($(this).next(".popover").length !== 0) {
+        $(this).popover('hide');
+      }
+      else {
+        $(".secondaryMenu [data-toggle=popover]").popover('hide');
+        $(this).popover('show');
+      }
+    });
   });
 })(jQuery);
