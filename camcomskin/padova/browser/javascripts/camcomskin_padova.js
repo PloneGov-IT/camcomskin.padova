71fab7b5ff09748d45a988f97955908a29ce1eff
Andrea Cecchi <andrea.cecchi85@gmail.com>
2015-09-30 14:49:04 +0200

    fix js

--- camcomskin/padova/browser/javascripts/camcomskin_padova.js
+++ camcomskin/padova/browser/javascripts/camcomskin_padova.js
@@ -16,21 +16,29 @@
     $(".secondaryMenu [data-toggle=popover]").popover({html:true});
     $("li.secondaryMenuItem a.popovers").click(function(e) {
       e.preventDefault();
+      //remove all class for open tab
+      $(".secondaryMenu li").removeClass("secondaryTabOpen");
       if ($(this).next(".popover").length !== 0) {
+        // the menu is already open, so close it
         $(this).popover('hide');
       }
       else {
+        //close all the menus before open the current one
         $(".secondaryMenu [data-toggle=popover]").popover('hide');
         $(this).popover('show');
+        $(this).parent().addClass("secondaryTabOpen");
       }
     });
 
     //handle general clicks, to close open menus or search box
     $(document).click(function(event) {
       if(!$(event.target).closest('.secondaryMenuItem').length) {
+        //close all secondary menus
         $(".secondaryMenu [data-toggle=popover]").popover('hide');
+        $(".secondaryMenu li").removeClass("secondaryTabOpen");
       }
       if(!$(event.target).closest('#camcomskin-searchbox').length && search_button[0] !== event.target) {
+        //close the search box
         $('#camcomskin-searchbox').slideUp();
       }
     });
