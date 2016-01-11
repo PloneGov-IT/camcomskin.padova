d1c320f84d77b90041adafcc9fe13835fdcd2d3e
Andrea Cecchi <andrea.cecchi85@gmail.com>
2016-01-11 13:51:12 +0100

    some fixes

--- camcomskin/padova/browser/menu_controlpanel.py
+++ camcomskin/padova/browser/menu_controlpanel.py
@@ -1,23 +1,9 @@
 # -*- coding: utf-8 -*-
 from collective.editablemenu import _
-from collective.editablemenu import logger
 from camcomskin.padova.interfaces import IEditableSecondaryMenuSettings
-from plone import api
-from plone.formwidget.contenttree import ContentTreeFieldWidget
-from plone.formwidget.contenttree import MultiContentTreeFieldWidget
-from plone.memoize import view
-from plone.z3cform.layout import wrap_form
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from z3c.form import button, form, field
-from zope.component import getMultiAdapter
-from zope.component import getMultiAdapter
-from zope.interface import Interface
-from zope.schema import Date, Choice
-from plone.app.textfield.value import RichTextValue
-from zope import schema
-import z3c.form.field
-from z3c.form.interfaces import HIDDEN_MODE
 from plone.app.registry.browser import controlpanel
+from collective.editablemenu.browser.menu_support_view import SubMenuDetailView
+
 
 class EditableSecondaryMenuSettingsEditForm(controlpanel.RegistryEditForm):
     """Media settings form.
@@ -31,3 +17,9 @@ class EditableSecondaryMenuSettingsView(controlpanel.ControlPanelFormWrapper):
     """Sitesearch settings control panel.
     """
     form = EditableSecondaryMenuSettingsEditForm
+
+
+class SecondaryMenuSupportView(SubMenuDetailView):
+    """
+    """
+    registry = "camcomskin.padova.interfaces.IEditableSecondaryMenuSettings.menu_tabs"
