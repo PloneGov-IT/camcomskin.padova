a84ae0ec69a70c0e4070a115563daafdc917d403
Andrea Cecchi <andrea.cecchi85@gmail.com>
2017-11-02 12:01:17 +0100

    add tiles views for homepages

--- camcomskin/padova/upgrades.py
+++ camcomskin/padova/upgrades.py
@@ -8,16 +8,6 @@ default_profile = 'profile-camcomskin.padova:default'
 REGISTRY_NAME = "camcomskin.padova.interfaces.IEditableSecondaryMenuSettings.menu_tabs"
 
 
-def to_1100(context):
-    """
-    delete old registry configuration and add a new one
-    """
-    setup_tool = getToolByName(context, 'portal_setup')
-    new_settings = generate_new_settings()
-    setup_tool.runImportStepFromProfile(default_profile, 'plone.app.registry')
-    api.portal.set_registry_record(REGISTRY_NAME, new_settings)
-
-
 def generate_new_settings():
     """
     """
@@ -38,3 +28,21 @@ def generate_new_settings():
         # we don't migrate additional columns because we can't know what's the common folder.
         new_settings.append(new_entry)
     return tuple(new_settings)
+
+
+def to_1100(context):
+    """
+    delete old registry configuration and add a new one
+    """
+    setup_tool = getToolByName(context, 'portal_setup')
+    new_settings = generate_new_settings()
+    setup_tool.runImportStepFromProfile(default_profile, 'plone.app.registry')
+    api.portal.set_registry_record(REGISTRY_NAME, new_settings)
+
+
+def to_2000(context):
+    """
+    new agid theme
+    """
+    setup_tool = getToolByName(context, 'portal_setup')
+    setup_tool.runImportStepFromProfile(default_profile, 'typeinfo', run_dependencies=True)
