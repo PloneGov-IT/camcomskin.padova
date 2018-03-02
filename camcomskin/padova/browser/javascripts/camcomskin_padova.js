aab1064e030db594601e640aef00934a6871e04a
nzambello <nicola.zambello@redturtle.it>
2018-03-02 18:28:56 +0100

    fix video and added slick for carousel

--- camcomskin/padova/browser/javascripts/camcomskin_padova.js
+++ camcomskin/padova/browser/javascripts/camcomskin_padova.js
@@ -32,27 +32,21 @@
 
     // return-to-top arrow
     $(window).scroll(function() {
-      if ($(window).width() < 768) {
-        // We only show the arrow on phones
-        if ($(this).scrollTop() >= 50) {
-          // If page is scrolled more than 50px
-          $('#return-to-top').fadeIn(200); // Fade in the arrow
-        } else {
-          $('#return-to-top').fadeOut(200); // Else fade out the arrow
-        }
+      if ($(this).scrollTop() >= 50) {
+        // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(200); // Fade in the arrow
+      } else {
+        $('#return-to-top').fadeOut(200); // Else fade out the arrow
       }
     });
     $('#return-to-top').click(function() {
       // When arrow is clicked
-      if ($(window).width() < 768) {
-        // We only show the arrow on phones
-        $('body,html').animate(
-          {
-            scrollTop: 0 // Scroll to top of body
-          },
-          500
-        );
-      }
+      $('body,html').animate(
+        {
+          scrollTop: 0, // Scroll to top of body
+        },
+        500
+      );
     });
 
     /*
@@ -119,6 +113,10 @@
       ) {
         $('#portal-mainnavigation').removeClass('open');
         $('body').removeClass('menuOpened');
+      }
+
+      if (!$(e.target).closest('.dropdown-languages').length) {
+        $('ul.languages').removeClass('open');
       }
     });
 
