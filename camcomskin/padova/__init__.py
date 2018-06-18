a85db36e3020e7d2afc22591559602d5f2b4e210
nzambello <nicola.zambello@redturtle.it>
2018-06-18 14:54:08 +0200

    override getPortlets

--- camcomskin/padova/__init__.py
+++ camcomskin/padova/__init__.py
@@ -1,8 +1,13 @@
 # -*- extra stuff goes here -*-
+from plone.portlets.retriever import PortletRetriever
 from zope.i18nmessageid import MessageFactory
+
+from .monkey import _myGetPortlets
 
 _ = MessageFactory('camcomskin.padova')
 
 
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
+
+    PortletRetriever.getPortlets = _myGetPortlets
